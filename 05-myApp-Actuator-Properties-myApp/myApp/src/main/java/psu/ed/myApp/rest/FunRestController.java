8183f65873ea444955fbe889ed6aaccbfeb9381a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-17 18:47:06 -0500

    Server port changed

--- 05-myApp-Actuator-Properties-myApp/myApp/src/main/java/psu/ed/myApp/rest/FunRestController.java
+++ 05-myApp-Actuator-Properties-myApp/myApp/src/main/java/psu/ed/myApp/rest/FunRestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-
 	
 //	INJECT properties for school.name, campus.location, campues.name
 	
