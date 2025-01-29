a11dec1b52cf283700b8b9502dda3c021f605a46
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-01-29 14:49:20 -0500

    Added createMultipleStudents method in CruddemoApplication

--- 03-JPA-CRUD/01-cruddemo-Create/cruddemo/src/main/java/psu/edu/cruddemo/CruddemoApplication.java
+++ 03-JPA-CRUD/01-cruddemo-Create/cruddemo/src/main/java/psu/edu/cruddemo/CruddemoApplication.java
@@ -27,12 +27,36 @@ public class CruddemoApplication {
     public CommandLineRunner commandLineRunner(StudentDao studentDAO) {
         return runner -> {
             createStudent(studentDAO);
+            createMultipleStudents(studentDAO);
+
         };
     }
 
-    private void createStudent(StudentDao studentDAO) {
+
+//  Will create many student 
+    private void createMultipleStudents(StudentDao studentDAO) {
+        System.out.println("Creating multiple student objects...");
+
+        Student student1 = new Student("John", "Doe", "john.doe@example.com");
+        Student student2 = new Student("Jane", "Doe", "jane.doe@example.com");
+        Student student3 = new Student("Alice", "Smith", "alice.smith@example.com");
+
+        System.out.println("Saving the students...");
+        studentDAO.save(student1);
+        studentDAO.save(student2);
+        studentDAO.save(student3);
+
+        System.out.println("Saved students. Generated IDs:");
+        System.out.println("Student 1 ID: " + student1.getId());
+        System.out.println("Student 2 ID: " + student2.getId());
+        System.out.println("Student 3 ID: " + student3.getId());
+    	
+	}
+
+//    Will create one student 
+	private void createStudent(StudentDao studentDAO) {
         System.out.println("Creating new student object...");
-        Student tempStudent = new Student("Sai", "Rangineeni", "sair@psu.edu");
+        Student tempStudent = new Student("Sai1", "Rangineeni2", "sair2@psu.edu");
 
         System.out.println("Saving the student...");
         studentDAO.save(tempStudent);
