751978a2d5ba8c42ed89034dd14dc9e3c41db643
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-05 13:17:34 -0500

    Added assignment 3

---
+++ 03-JPA-CRUD/Assignment 3/book/src/main/java/psu/edu/book/MyAssignmentThreeApplication.java
@@ -0,0 +1,13 @@
+package psu.edu.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAssignmentThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAssignmentThreeApplication.class, args);
+	}
+
+}
