53968c3310eb00028080085a6fbfcdba0f1d9273
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-10 11:11:37 -0500

    Added StudentRestController with @PostConstruct for initial data loading

---
+++ 04-Rest-crud/03-Rest-CRUD-STUDENT-LIST-Refactor/demo/src/main/java/psu/edu/demo/entity/Student.java
@@ -0,0 +1,43 @@
+package psu.edu.demo.entity;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    // Default constructor
+    public Student() {
+    }
+
+    // Parameterized constructor
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
