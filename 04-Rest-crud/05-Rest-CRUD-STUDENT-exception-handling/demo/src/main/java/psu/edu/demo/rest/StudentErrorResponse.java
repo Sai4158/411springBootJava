a0b46a4b3b17ac477489be63c966cd379e1e0946
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-12 10:50:15 -0500

    Added exception handling for StudentNotFoundException with @ExceptionHandler

---
+++ 04-Rest-crud/05-Rest-CRUD-STUDENT-exception-handling/demo/src/main/java/psu/edu/demo/rest/StudentErrorResponse.java
@@ -0,0 +1,44 @@
+package psu.edu.demo.rest;
+
+public class StudentErrorResponse {
+    
+    private int status;
+    private String message;
+    private long timestamp;
+
+    public StudentErrorResponse() {
+    	
+    }
+
+    // Parameterized constructor
+    public StudentErrorResponse(int status, String message, long timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    // Getters and Setters
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
--- 04-Rest-crud/05-Rest-CRUD-STUDENT-exception-handling/demo/src/main/java/psu/edu/demo/rest/StudentRestController.java
+++ 04-Rest-crud/05-Rest-CRUD-STUDENT-exception-handling/demo/src/main/java/psu/edu/demo/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package psu.edu.demo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +41,28 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}") 
     public Student getStudents(@PathVariable int studentId) {
 
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+    	
         return theStudents.get(studentId); 
     }
+    
+    //exception
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+    
+    	  // Create StudentErrorResponse object
+        StudentErrorResponse error = new StudentErrorResponse();
+        
+        // Set error details
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // Return ResponseEntity with error object and HTTP status
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    
+    }
+//    
 }
