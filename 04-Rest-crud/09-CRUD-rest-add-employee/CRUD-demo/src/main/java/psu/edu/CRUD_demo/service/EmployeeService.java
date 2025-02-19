73f1f90f8157b21ca817720a21a2a5eb132d8f66
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-19 10:55:08 -0500

    Added post method

---
+++ 04-Rest-crud/09-CRUD-rest-add-employee/CRUD-demo/src/main/java/psu/edu/CRUD_demo/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package psu.edu.CRUD_demo.service;
+
+import java.util.List;
+import psu.edu.CRUD_demo.entity.Employee;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int theId);
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
+}
