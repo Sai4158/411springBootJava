2bf1044fd0635bd954df5676b2ea88a55e4b7b13
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-21 10:25:22 -0500

    Corrected code

---
+++ 04-Rest-crud/010-CRUD-UpdateAndDeleteEmployee/CRUD-demo/src/main/java/psu/edu/CRUD_demo/service/EmployeeIMPL.java
@@ -0,0 +1,46 @@
+package psu.edu.CRUD_demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import psu.edu.CRUD_demo.dao.EmployeeDAO;
+import psu.edu.CRUD_demo.entity.Employee;
+
+@Service
+public class EmployeeIMPL implements EmployeeService {
+
+    private final EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeIMPL(EmployeeDAO theEmployeeDAO) {
+        this.employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+	@Override
+	public Employee findById(int theId) {
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public Employee save(Employee theEmployee) {
+		
+		return employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		
+		employeeDAO.deleteById(theId);
+		
+	}
+}
