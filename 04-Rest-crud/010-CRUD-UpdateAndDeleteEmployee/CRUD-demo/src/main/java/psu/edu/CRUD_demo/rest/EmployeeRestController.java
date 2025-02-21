553b6bcd089e02f9e7697e9586bd4eb4c546ea9c
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-02-21 10:51:37 -0500

    Added put and delete methods

--- 04-Rest-crud/010-CRUD-UpdateAndDeleteEmployee/CRUD-demo/src/main/java/psu/edu/CRUD_demo/rest/EmployeeRestController.java
+++ 04-Rest-crud/010-CRUD-UpdateAndDeleteEmployee/CRUD-demo/src/main/java/psu/edu/CRUD_demo/rest/EmployeeRestController.java
@@ -1,9 +1,11 @@
 package psu.edu.CRUD_demo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,5 +57,27 @@ public class EmployeeRestController {
  		return dbEmployee;
  	}
  	
- 	
+ // PUT "/employees" - Update employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+    
+ // add mapping for DELETE "/employees/{employeeId}" - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if (tempEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
+    }
+
+
+  
 }
