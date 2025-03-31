cdf9ce7837819731112e9b08a3ad7ce8c87496dd
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-03-31 12:40:17 -0400

    added Employee CRUD with Thymeleaf, styled UI with Bootstrap, integrated DealScape branding, validated forms

---
+++ midterm2-Thymeleaf/thymeleafdemo/src/main/java/psu/edu/quiz/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package psu.edu.quiz.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import psu.edu.quiz.entity.Employee;
+
+public interface EmployeeRepository  extends JpaRepository<Employee, Integer>{
+
+//	THATS IT FOR THIS CODE
+	
+	List<Employee> findAllByOrderByNameAsc();
+
+}
---
+++ midterm2-Thymeleaf/thymeleafdemo/src/main/java/psu/edu/quiz/entity/Employee.java
@@ -0,0 +1,102 @@
+package psu.edu.quiz.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employeedatatable")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "e_id")			
+    private int id;
+
+    @Column(name = "e_name")
+    private String name;
+
+    @Column(name = "e_salary")
+    private double salary;
+
+    @Column(name = "e_age")
+    private int age;
+
+    @Column(name = "e_gender")
+    private String gender;
+
+    @Column(name = "e_dept")
+    private String department;
+
+    // Default Constructor
+    public Employee() {
+    }
+
+    // Parameterized Constructor
+    public Employee(String name, double salary, int age, String gender, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.age = age;
+        this.gender = gender;
+        this.department = department;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
