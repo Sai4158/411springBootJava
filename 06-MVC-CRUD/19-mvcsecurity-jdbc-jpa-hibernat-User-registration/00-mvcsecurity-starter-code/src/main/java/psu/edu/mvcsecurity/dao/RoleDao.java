1c84ffa233df5348246804d634fb757933e29962
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-04-25 10:18:02 -0400

    Deleted the wrogn code

--- 06-MVC-CRUD/19-mvcsecurity-jdbc-jpa-hibernat-User-registration/00-mvcsecurity-starter-code/src/main/java/psu/edu/mvcsecurity/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package psu.edu.mvcsecurity.dao;
-
-import psu.edu.mvcsecurity.entity.Role;
-
-public interface RoleDao {
-
-	public Role findRoleByName(String theRoleName);
-	
-}
--- 06-MVC-CRUD/19-mvcsecurity-jdbc-jpa-hibernat-User-registration/00-mvcsecurity-starter-code/src/main/java/psu/edu/mvcsecurity/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package psu.edu.mvcsecurity.dao;
-
-import psu.edu.mvcsecurity.entity.User;
-
-public interface UserDao {
-
-    User findByUserName(String userName);
-    
-}
