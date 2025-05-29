73df3fe51e55b7842572fb789edab92295902af0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-05-29 18:15:46 -0400

    Spring boot recap

---
+++ 0_Recap/0_Summer/demo/src/main/java/Fun/summer/demo/SummerApplication.java
@@ -0,0 +1,13 @@
+package Fun.summer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerApplication.class, args);
+	}
+
+}
---
+++ 0_Recap/0_Summer/demo/src/test/java/Fun/summer/demo/SummerApplicationTests.java
@@ -0,0 +1,13 @@
+package Fun.summer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
