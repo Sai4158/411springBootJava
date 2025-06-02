fc5c2d46d83dd16dafd8358eb428927853354e4a
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-06-01 22:49:35 -0400

    IOC Container & Dependency Injection in Spring Boot (Autowired and component)

---
+++ 0_Recap/0_Summer/demo/src/main/java/Fun/summer/demo/controller/test/Dog.java
@@ -0,0 +1,13 @@
+package Fun.summer.demo.controller.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog {
+
+	public String fun() {
+		return "somthing";
+		
+	}
+	
+}
