1bf5b96f4deb5b052d6f3fab3bbf50ed9a6c8299
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-06-02 20:10:41 -0400

    Added health check to journal entry app

---
+++ 0_Recap/1_JournalApp/Journal-App/src/main/java/net/sai/controller/HealthCheck.java
@@ -0,0 +1,15 @@
+package net.sai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+	@GetMapping("/healthCheck")
+	public String healthCheck() {
+		
+		return "ok";
+	
+	}
+}
