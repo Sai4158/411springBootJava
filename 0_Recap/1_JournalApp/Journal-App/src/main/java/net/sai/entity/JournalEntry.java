57774f944c9766102031257b5facc6cc4a9b579e
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-06-20 18:57:49 -0400

    Added Lombok

--- 0_Recap/1_JournalApp/Journal-App/src/main/java/net/sai/entity/JournalEntry.java
+++ 0_Recap/1_JournalApp/Journal-App/src/main/java/net/sai/entity/JournalEntry.java
@@ -6,41 +6,16 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
-
 	@Id
-	private ObjectId id;
-	
+	private ObjectId id;	
 	private String title;
 	private Date date;
 	private String content;
-//	Get and set methods 
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public ObjectId getId() {
-		return id;
-	}
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	
-	
+
 }
