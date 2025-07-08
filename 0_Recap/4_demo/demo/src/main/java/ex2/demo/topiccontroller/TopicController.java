2c881b351f25b793e91e2ce0cfea78c83a9b9e09
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-07-08 01:34:04 -0400

    Add endpoint to fetch a single topic by ID

--- 0_Recap/4_demo/demo/src/main/java/ex2/demo/topiccontroller/TopicController.java
+++ 0_Recap/4_demo/demo/src/main/java/ex2/demo/topiccontroller/TopicController.java
@@ -1,6 +1,7 @@
 package ex2.demo.topiccontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -16,4 +17,12 @@ public class TopicController {
     public List<Topics> getAllTopics() {
         return topicService.getAlltopics();
     }
+    
+    
+    @RequestMapping("/topics/{id}")
+    public Topics getOneTopic(@PathVariable String id) {
+        return topicService.getOneTopic(id);
+    }
+    
+    
 }
--- 0_Recap/4_demo/demo/src/main/java/ex2/demo/topiccontroller/TopicService.java
+++ 0_Recap/4_demo/demo/src/main/java/ex2/demo/topiccontroller/TopicService.java
@@ -4,21 +4,30 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class TopicService {
 
 	private List<Topics> Topics1 = Arrays.asList(
-            new Topics("1", "Java", "Core Java Concepts"),
-            new Topics("2", "Spring Boot", "Spring Boot Basics"),
-            new Topics("3", "JavaScript", "Frontend Scripting"),
-            new Topics("4", "Python", "Data & Scripting"),
-            new Topics("5", "SQL", "Database ")
-        );
-	
-	
+			new Topics("1", "Java", "Core Java Concepts"),
+			new Topics("2", "Spring Boot", "Spring Boot Basics"),
+			new Topics("3", "JavaScript", "Frontend Scripting"),
+			new Topics("4", "Python", "Data & Scripting"),
+			new Topics("5", "SQL", "Database ")
+			);
+
+
 	public List<Topics> getAlltopics(){
 		return Topics1;
 	}
-	
+
+	public Topics getOneTopic(String id){
+
+
+		return Topics1.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+
+
+	}
+
 }
