79e912abe6e1a4bfd5bf7eaf104ba84e49237463
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2025-07-09 20:15:56 -0400

    Refactor topic add and retrieval methods

--- 0_Recap/4_demo/demo/src/main/java/ex2/demo/topiccontroller/TopicController.java
+++ 0_Recap/4_demo/demo/src/main/java/ex2/demo/topiccontroller/TopicController.java
@@ -12,24 +12,29 @@ import java.util.Arrays;
 @RestController
 public class TopicController {
 
+//	Auto wired for the topic service 
 	@Autowired
 	private TopicService topicService;
+
 	
+	// gets all the topics 
     @RequestMapping("/topics")
     public List<Topics> getAllTopics() {
         return topicService.getAlltopics();
     }
     
-    
+//    Gets topic based on the id
     @RequestMapping("/topics/{id}")
     public Topics getOneTopic(@PathVariable String id) {
         return topicService.getOneTopic(id);
     }
     
+    
+    // will post a topic
     @PostMapping("/topics")
-    public String AddTopic(@RequestBody Topics topic) {
+    public void AddTopic(@RequestBody Topics topic) {
         
-        return topicService.AddTopic;
+        topicService.AddTopic(topic);
     }
     
     
